2efc4bc522069fc854486b63f6be9144a9036ab3
Brian-ED <brianellingsgaard9@gmail.com>
2024-07-03 20:01:26 +0100

    car_game clensed away to a square drawing
    Co-authored-by: M1kiMinaj <M1kiMinaj@users.noreply.github.com>

--- car_game.py
+++ car_game.py
@@ -1,101 +1,63 @@
+# import the pygame module
 import pygame
-from pygame.locals import *
-from OpenGL.GL import *
-from OpenGL.GLU import *
-import math
-import sys
 
-# Initialize Pygame
+# import pygame.locals for easier
+# access to key coordinates
+from pygame.locals import *
+
+# Define our square object and call super to
+# give it all the properties and methods of pygame.sprite.Sprite
+# Define the class for our square objects
+class Square(pygame.sprite.Sprite):
+    def __init__(self):
+        super(Square, self).__init__()
+
+        # Define the dimension of the surface
+        # Here we are making squares of side 25px
+        self.surf = pygame.Surface((25, 25))
+
+        # Define the color of the surface using RGB color coding.
+        self.surf.fill((0, 200, 255))
+        self.rect = self.surf.get_rect()
+
+# initialize pygame
 pygame.init()
 
-# Set up display
-display = (800, 600)
-pygame.display.set_mode(display, DOUBLEBUF | OPENGL)
-glViewport(0, 0, display[0], display[1])
+# Define the dimensions of screen object
+screen = pygame.display.set_mode((800, 600))
 
-# Set up perspective
-gluPerspective(45, (display[0] / display[1]), 0.1, 50.0)
-glTranslatef(0.0, 0.0, -5)
+# instantiate all square objects
+square1 = Square()
+square2 = Square()
+square3 = Square()
+square4 = Square()
 
-# Set up clear color
-glClearColor(0.0, 0.0, 0.0, 1.0)
+# Variable to keep our game loop running
+gameOn = True
 
-# Enable depth testing
-glEnable(GL_DEPTH_TEST)
-
-# Initial car parameters
-car_speed = 0.0
-car_turn_angle = 0.0
-car_pos = [0.0, 0.0, 0.0]
-car_direction = 0.0
-
-# Function to draw the car
-def draw_car():
-    glBegin(GL_QUADS)
-
-    # Car body
-    glColor3f(1.0, 0.0, 0.0)  # Red
-    glVertex3f(1.0, 0.0, -1.0)
-    glVertex3f(1.0, 0.0, 1.0)
-    glVertex3f(-1.0, 0.0, 1.0)
-    glVertex3f(-1.0, 0.0, -1.0)
-
-    # Car top
-    glColor3f(0.0, 1.0, 0.0)  # Green
-    glVertex3f(1.0, 0.0, -1.0)
-    glVertex3f(1.0, 0.0, 1.0)
-    glVertex3f(0.0, 1.0, 1.0)
-    glVertex3f(0.0, 1.0, -1.0)
-
-    glVertex3f(-1.0, 0.0, 1.0)
-    glVertex3f(-1.0, 0.0, -1.0)
-    glVertex3f(0.0, 1.0, -1.0)
-    glVertex3f(0.0, 1.0, 1.0)
-
-    glEnd()
-
-# Main game loop
-while True:
+# Our game loop
+while gameOn:
+    # for loop through the event queue
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
 
-        # Keyboard input handling
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_w:
-                car_speed += 0.1  # Accelerate
-            elif event.key == pygame.K_s:
-                car_speed -= 0.05  # Decelerate (half as fast)
-            elif event.key == pygame.K_a:
-                car_turn_angle += 5.0  # Turn left
-            elif event.key == pygame.K_d:
-                car_turn_angle -= 5.0  # Turn right
+        # Check for KEYDOWN event
+        if event.type == KEYDOWN:
 
-    # Limit speed to a maximum of 1
-    car_speed = min(max(car_speed, 0), 1)
+            # If the Backspace key has been pressed set
+            # running to false to exit the main loop
+            if event.key == K_BACKSPACE:
+                gameOn = False
 
-    # Update car direction based on turn angle
-    car_direction += car_turn_angle * 0.01  # Reduce turn angle impact
+        # Check for QUIT event
+        elif event.type == QUIT:
+            gameOn = False
 
-    # Update car position based on speed and direction
-    car_pos[0] += car_speed * math.sin(math.radians(car_direction))
-    car_pos[2] += car_speed * math.cos(math.radians(car_direction))
+    # Define where the squares will appear on the screen
+    # Use blit to draw them on the screen surface
+    screen.blit(square1.surf, (40, 40))
+    screen.blit(square2.surf, (40, 530))
+    screen.blit(square3.surf, (730, 40))
+    screen.blit(square4.surf, (730, 530))
 
-    # Clear the screen
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-
-    # Reset transformations
-    glLoadIdentity()
-    glTranslatef(0.0, 0.0, -5)
-    glTranslatef(car_pos[0], car_pos[1], car_pos[2])
-    glRotatef(car_direction, 0, 1, 0)
-
-    # Draw the car
-    draw_car()
-
-    # Update display
+    # Update the display using flip
     pygame.display.flip()
-
-    # Pause briefly to control frame rate
-    pygame.time.wait(10)
